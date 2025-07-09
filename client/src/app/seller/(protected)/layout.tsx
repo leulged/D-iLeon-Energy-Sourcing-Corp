1bd413391fdd270980cf9df5e670b792d55ef47f
Leul Gedion <leulgedion224@gmail.com>
2025-07-09 09:21:24 +0300

    resolve conflict issues

--- client/src/app/seller/(protected)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import ProtectedRoute from "@/app/components/Protected";
-
-export default function RootLayout({
-	children,
-}: Readonly<{
-	children: React.ReactNode;
-}>) {
-	return <ProtectedRoute roles={["seller"]}>{children}</ProtectedRoute>;
-}
